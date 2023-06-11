77d1d62dfa1caf94e64e83fd1412184582ba5038
Asma Farooq <asma.faarook@gmail.com>
2023-06-10 18:00:24 -0700

    Login-Logout-Register

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -14,38 +14,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ecommerce.dto.ProductDto;
 import com.ecommerce.model.Category;
+import com.ecommerce.model.Customer;
 import com.ecommerce.service.CategoryService;
 import com.ecommerce.service.ProductService;
 
-
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
 @Controller
 public class HomeController {
-    @Autowired
-    private ProductService productService;
+	
+	private ProductService productService;
+	private CategoryService customerService;
 
-    @Autowired
-    private CategoryService categoryService;
+	@Autowired
+	private CategoryService categoryService;
 
-   
+	@RequestMapping(value = { "/index", "/" }, method = RequestMethod.GET)
+	public String home(Model model, Principal principal, HttpSession session) {
 
-    @RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
-    public String home(Model model, Principal principal, HttpSession session){
-		/*
-		 * if(principal != null){ session.setAttribute("username", principal.getName());
-		 * Customer customer = customerService.findByUsername(principal.getName());
-		 * ShoppingCart cart = customer.getShoppingCart();
-		 * session.setAttribute("totalItems", cart.getTotalItems()); }else{
-		 * session.removeAttribute("username"); }
-		 */
-        return "home";
-    }
+		if (principal != null) {
+			session.setAttribute("username", principal.getName());
+			//Customer customer = customerService.findByUsername(principal.getName());
+			//ShoppingCart cart = customer.getShoppingCart();
+			//session.setAttribute("totalItems", cart.getTotalItems());
+		} else {
+			session.removeAttribute("username");
+		}
 
-    @GetMapping("/home")
-    public String index(Model model){
-        List<Category> categories = categoryService.findAll();
-        List<ProductDto> productDtos = productService.findAll();
-        model.addAttribute("categories", categories);
-        model.addAttribute("products", productDtos);
-        return "index";
-    }
+		return "home";
+	}
+
+	@GetMapping("/home")
+	public String index(Model model) {
+		List<Category> categories = categoryService.findAll();
+		List<ProductDto> productDtos = productService.findAll();
+		model.addAttribute("categories", categories);
+		model.addAttribute("products", productDtos);
+		return "index";
+	}
 }
